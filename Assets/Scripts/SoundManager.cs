80d1bc37b6a4014ce5e9f74eca81b4bb6a1610a5
arshiatn <151800419+arshiatn@users.noreply.github.com>
2025-07-16 17:07:41 +0200

    SoundManager Don'tDestroy weg

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -30,7 +30,6 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Optional: persist across scenes
         }
         else
         {
